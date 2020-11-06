867f417b37ed4b6866d942df2831450dc205cb5b
Peter <pgkutc@gmail.com>
2020-11-05 21:07:25 -0500

    added properties to vitamin/mineral models

--- src/main/java/models/Minerals.java
+++ src/main/java/models/Minerals.java
@@ -1,4 +1,8 @@
 package models;
 
 public class Minerals {
+    private String name;
+    private String brand;
+    private int qty;
+    private float price;
 }
--- src/main/java/models/Vitamins.java
+++ src/main/java/models/Vitamins.java
@@ -1,4 +1,8 @@
 package models;
 
 public class Vitamins {
+    private String name;
+    private String brand;
+    private int qty;
+    private float price;
 }
